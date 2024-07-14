b9113762dc1a36be06c76421aaf26f99d343ddfd
Hector Martinez <hec.gre81@gmail.com>
2024-07-14 02:40:00 -0600

    Initialize function each time the server starts

--- app.py
+++ app.py
@@ -24,16 +24,18 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Extensiones permitidas
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+# Face Recognition Variables
+reference_encodings = []
+reference_names = []
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/initialize', methods=['POST'])
 def initialize():
-    data = request.json
     reference_directory = 'reconocimiento'
     global reference_encodings, reference_names
     reference_encodings, reference_names = initialize_categories(reference_directory)
-    return jsonify({"status": "Categorías inicializadas"})
+    print({"status": "Categorías inicializadas"})
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -63,6 +65,16 @@ def upload_file():
             'is_primary': False
         })
         
+        # json.dump(file_contents, open('db.json', 'w'), indent=4)
+
+        # Categorizar la imagen
+        category = get_image_category(file_path, reference_encodings, reference_names)
+
+        for image in file_contents['images']:
+            if image['filename'] == filename:
+                image['classification'] = category
+                break
+
         json.dump(file_contents, open('db.json', 'w'), indent=4)
     
         return jsonify({'message': 'File uploaded successfully', 'filename': filename}), 200
@@ -78,4 +90,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    initialize()
     app.run(debug=True)
